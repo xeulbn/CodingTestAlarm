766c3a1f9e907372afd95d4e7c7e61994cf5bae3
xeulbn <gsbtiger0215@gmail.com>
2025-09-29 19:40:32 +0900

    Feat : Initial Commit

---
+++ src/main/java/org/codingtestalarm/CodingTestAlarmApplication.java
@@ -0,0 +1,13 @@
+package org.codingtestalarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingTestAlarmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodingTestAlarmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/codingtestalarm/CodingTestAlarmApplicationTests.java
@@ -0,0 +1,13 @@
+package org.codingtestalarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingTestAlarmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
